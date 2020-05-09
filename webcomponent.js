50680048a53c162d5c9c1a48ba9b6bd3d5333832
kgrzymala <65021331+kgrzymala@users.noreply.github.com>
2020-05-09 13:57:55 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -43,15 +43,9 @@ customElements.define('com-sap-sample-template', class WidgetTemplate extends HT
             this.redraw();
         }
 
-         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-        disconnectedCallback(){
-        
-        }
+       
 
-         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-		onCustomWidgetBeforeUpdate(oChangedProperties) {
-
-		}
+         
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
@@ -60,20 +54,7 @@ customElements.define('com-sap-sample-template', class WidgetTemplate extends HT
             }
         }
         
-        //When the custom widget is removed from the canvas or the analytic application is closed
-        onCustomWidgetDestroy(){
-        }
-
-        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
-        /*
-        onCustomWidgetResize(width, height){
-        
-        }
-        */
-
-        redraw(){
+            redraw(){
         }
     
     
