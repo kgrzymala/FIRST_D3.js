9122cd327a54bd78e4e1cf9d50716ad3f66bc8b6
kgrzymala <65021331+kgrzymala@users.noreply.github.com>
2020-05-09 17:08:05 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -35,6 +35,15 @@ class WidgetTemplate extends HTMLElement {
         connectedCallback(){
             this._firstConnection = true;
             this.redraw();
+		
+	<script>       
+d3
+  .select(".target")  // select the elements that have the class 'target'
+  .style("stroke-width", 10) // change their style: stroke width is not equal to 8 pixels
+</script>	
+		
+		
+		
         }
 
        
@@ -46,11 +55,7 @@ class WidgetTemplate extends HTMLElement {
             if (this._firstConnection){
                 this.redraw();
             }
-<script>       
-d3
-  .select(".WidgetTemplate")  // select the elements that have the class 'target'
-  .style("stroke-width", 10) // change their style: stroke width is not equal to 8 pixels
-</script>		
+		
 		}
         
             redraw(){
