118d5baf9ee3cf701ed505900173c0cfce2e4949
kgrzymala <65021331+kgrzymala@users.noreply.github.com>
2020-05-09 14:06:57 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -11,5 +11,41 @@ let tmpl = document.createElement('template');
   <circle class="target" style="fill: #69b3a2" stroke="black" cx=50 cy=50 r=40></circle>
 </svg>
     `;
+    
+    customElements.define('com-sap-sample-template', class WidgetTemplate extends HTMLElement {
+
+
+		constructor() {
+			super(); 
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(tmpl.content.cloneNode(true));
+			
+		
+		}
+
+
+        //Fired when the widget is added to the html DOM of the page
+        connectedCallback(){
+            this._firstConnection = true;
+            this.redraw();
+        }
+
+       
+
+         
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+		onCustomWidgetAfterUpdate(oChangedProperties) {
+            if (this._firstConnection){
+                this.redraw();
+            }
+        }
+        
+            redraw(){
+        }
+    
+    
+    });
+    
         
 })();
