ed1dea651ca8b2f84b41068ea5361bf7a3133335
kgrzymala <65021331+kgrzymala@users.noreply.github.com>
2020-05-09 16:13:59 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -46,11 +46,11 @@ class WidgetTemplate extends HTMLElement {
             if (this._firstConnection){
                 this.redraw();
             }
-       
+<script>       
 d3
   .select(".target")  // select the elements that have the class 'target'
   .style("stroke-width", 10) // change their style: stroke width is not equal to 8 pixels
-		
+</script>		
 		}
         
             redraw(){
