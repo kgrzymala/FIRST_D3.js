597deaf8ecfff44b7b0203c00bc81fe8feee8ce4
kgrzymala <65021331+kgrzymala@users.noreply.github.com>
2020-05-11 17:10:50 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -144,7 +144,16 @@
 
       var shadow = window.getSelection(this._shadowRoot);
       this._tagContainer = document.createElement(this._tagType);
-      var theText = document.createTextNode("taki huj");    
+      var theText = document.createTextNode("taki huj");
+      var myCircle = document.createElementNS(svgNS,"circle"); //to create a circle. for rectangle use "rectangle"
+      myCircle.setAttributeNS(null,"id","mycircle");
+      myCircle.setAttributeNS(null,"cx",100);
+      myCircle.setAttributeNS(null,"cy",100);
+      myCircle.setAttributeNS(null,"r",50);
+      myCircle.setAttributeNS(null,"fill","black");
+      myCircle.setAttributeNS(null,"stroke","none");
+
+      this._tagContainer.appendChild(myCircle);
       this._tagContainer.appendChild(theText); 
       this._shadowRoot.appendChild(this._tagContainer);
 
