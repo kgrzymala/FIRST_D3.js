33c0debdb05c5c2916879e841fb383ab41744deb
kgrzymala <65021331+kgrzymala@users.noreply.github.com>
2020-05-11 16:46:57 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -144,7 +144,7 @@
 
       var shadow = window.getSelection(this._shadowRoot);
       this._tagContainer = document.createElement(this._tagType);
-      var theText = document.createTextNode(this._tagText);    
+      var theText = document.createTextNode("taki huj");    
       this._tagContainer.appendChild(theText); 
       this._shadowRoot.appendChild(this._tagContainer);
 
